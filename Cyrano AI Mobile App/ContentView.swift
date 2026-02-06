2a070562960fd5ba7f9658f1e4c22853a053e756
Aryan Goel <workingaryan3@gmail.com>
2026-02-05 18:06:24 -0800

    Initial Commit

---
+++ Cyrano AI Mobile App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cyrano AI Mobile App
+//
+//  Created by Aryan Goel on 2/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cyrano AI Mobile App/Cyrano_AI_Mobile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cyrano_AI_Mobile_AppApp.swift
+//  Cyrano AI Mobile App
+//
+//  Created by Aryan Goel on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Cyrano_AI_Mobile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
